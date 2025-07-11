4705e698d1541129a36c44b2346261c2b8b41966
BabyNewCode <154010779+BabyNewCode@users.noreply.github.com>
2025-07-11 16:48:45 +0200

    Update AttractionController.java

--- parc-attraction-api/src/main/java/com/parc/controller/AttractionController.java
+++ parc-attraction-api/src/main/java/com/parc/controller/AttractionController.java
@@ -1,10 +1,8 @@
 package com.parc.controller;
 
 import com.parc.model.Attraction;
+import com.parc.repository.AttractionRepository;
 import com.parc.service.AttractionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,48 +11,31 @@ import java.util.List;
 @RequestMapping("/api/attractions")
 public class AttractionController {
 
-    @Autowired
-    private AttractionService attractionService;
+    private final AttractionRepository repo;
+    private final AttractionService service;
+
+    public AttractionController(AttractionRepository repo, AttractionService service) {
+        this.repo = repo;
+        this.service = service;
+    }
 
     @GetMapping
-    public ResponseEntity<List<Attraction>> getAllAttractions() {
-        List<Attraction> attractions = attractionService.findAll();
-        return new ResponseEntity<>(attractions, HttpStatus.OK);
+    public List<Attraction> getAll() {
+        return repo.findAll();
     }
 
     @PostMapping
-    public ResponseEntity<Attraction> createAttraction(@RequestBody Attraction attraction) {
-        Attraction createdAttraction = attractionService.save(attraction);
-        return new ResponseEntity<>(createdAttraction, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Attraction> getAttractionById(@PathVariable Long id) {
-        Attraction attraction = attractionService.findById(id);
-        return new ResponseEntity<>(attraction, HttpStatus.OK);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Attraction> updateAttraction(@PathVariable Long id, @RequestBody Attraction attraction) {
-        Attraction updatedAttraction = attractionService.update(id, attraction);
-        return new ResponseEntity<>(updatedAttraction, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAttraction(@PathVariable Long id) {
-        attractionService.delete(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public Attraction create(@RequestBody Attraction a) {
+        return repo.save(a);
     }
 
     @PatchMapping("/{id}/close")
-    public ResponseEntity<Void> closeAttraction(@PathVariable Long id) {
-        attractionService.closeAttraction(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public void fermer(@PathVariable Long id) {
+        service.fermer(id);
     }
 
     @PatchMapping("/{id}/open")
-    public ResponseEntity<Void> openAttraction(@PathVariable Long id) {
-        attractionService.openAttraction(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public void ouvrir(@PathVariable Long id) {
+        service.ouvrir(id);
     }
 }
