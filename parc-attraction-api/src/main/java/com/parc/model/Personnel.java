36b6e3c622f6a686e3bbeb69d9fc219afcae6b26
BabyNewCode <154010779+BabyNewCode@users.noreply.github.com>
2025-07-11 16:28:54 +0200

    Update Personnel.java

--- parc-attraction-api/src/main/java/com/parc/model/Personnel.java
+++ parc-attraction-api/src/main/java/com/parc/model/Personnel.java
@@ -1,53 +1,13 @@
 package com.parc.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class Personnel {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
+    @Id @GeneratedValue private Long id;
     private String nom;
-    
     private String role;
-    
+
     @ManyToOne
-    @JoinColumn(name = "restaurant_id")
     private Restaurant restaurant;
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Restaurant getRestaurant() {
-        return restaurant;
-    }
-
-    public void setRestaurant(Restaurant restaurant) {
-        this.restaurant = restaurant;
-    }
 }
