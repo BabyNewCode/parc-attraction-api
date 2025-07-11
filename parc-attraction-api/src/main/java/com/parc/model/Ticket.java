bb89d60ffa21f72c2df245c5b58a5731d7480c35
BabyNewCode <154010779+BabyNewCode@users.noreply.github.com>
2025-07-11 16:27:56 +0200

    Update Ticket.java

--- parc-attraction-api/src/main/java/com/parc/model/Ticket.java
+++ parc-attraction-api/src/main/java/com/parc/model/Ticket.java
@@ -1,43 +1,12 @@
 package com.parc.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class Ticket {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    @Id @GeneratedValue private Long id;
     private double prix;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
     private User acheteur;
-
-    // Getters and Setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getPrix() {
-        return prix;
-    }
-
-    public void setPrix(double prix) {
-        this.prix = prix;
-    }
-
-    public User getAcheteur() {
-        return acheteur;
-    }
-
-    public void setAcheteur(User acheteur) {
-        this.acheteur = acheteur;
-    }
 }
