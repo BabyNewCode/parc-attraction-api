734f54a5065deed06827d324043acf70da7c843a
BabyNewCode <154010779+BabyNewCode@users.noreply.github.com>
2025-07-11 16:28:38 +0200

    Update Restaurant.java

--- parc-attraction-api/src/main/java/com/parc/model/Restaurant.java
+++ parc-attraction-api/src/main/java/com/parc/model/Restaurant.java
@@ -1,42 +1,13 @@
 package com.parc.model;
 
-import javax.persistence.*;
-import java.util.Set;
+import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 public class Restaurant {
-    
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+    @Id @GeneratedValue private Long id;
     private String nom;
 
     @OneToMany(mappedBy = "restaurant")
-    private Set<Personnel> personnel;
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public Set<Personnel> getPersonnel() {
-        return personnel;
-    }
-
-    public void setPersonnel(Set<Personnel> personnel) {
-        this.personnel = personnel;
-    }
+    private List<Personnel> equipe;
 }
